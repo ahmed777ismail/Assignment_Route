5c6db01ebaed0ed7d1ff6fd740c6f4c77da0448b
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-06-09 22:27:24 +0300

    Add files via upload
    Assignment 2 C#

---
+++ ass2 C#/ConsoleApp1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region assignment 02
+            #region Q1: Write a program that allows the user to enter a number then print it
+            #endregion
+
+            #region Q2: Write C# program that Convert a string to an integer, but the string contains non-numeric characters. And mention what will happen
+            //string input = "123mmm";
+            //int n = int.Parse(input);
+            //Console.WriteLine(n);
+
+            #endregion
+
+            #region Q3: Write C# program that Perform a simple arithmetic operation with floating-point numbers And mention what will happen
+
+            //double a = 0.1;
+            //double b = 0.2;
+            //double sum = a + b;
+            //Console.WriteLine(sum); // 0.3
+
+
+            #endregion
+
+            #region Q4: Write C# program that Extract a substring from a given
+            //string originalString = "hello world";
+            //string substring = originalString.Substring(0, 5);
+            //Console.WriteLine(substring);
+            #endregion
+
+            #region Q5: Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+            //int a = 5;
+            //int b = 10;
+            //a = b;
+            //b = 20;
+            //Console.WriteLine("a = " + a);
+            //Console.WriteLine("b = " + b);
+            #endregion
+
+            #region Q6: Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen
+            //int[] arr1 = {1,2,3 };
+            //int[] arr2 = arr1;
+            //arr2[0] = 100;
+            //Console.WriteLine("arr1[0]" + arr1[0]);
+            //Console.WriteLine("arr2[0]" + arr2[0]);
+
+            #endregion
+
+            #region Q7: Write C# program that take two string variables and print them as one variable 
+            //string s1, s2, s3;
+            //s1 = Console.ReadLine();
+            //s2 = Console.ReadLine();
+            //s3 = s1 + s2;
+            //Console.WriteLine(s3);
+
+            #endregion
+
+            #region Q8: 8-	Which of the following statements is correct about the C#.NET code snippet given below?
+
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+
+            //a)	A value 0 will be assigned to d.
+            //b)	A value 1 will be assigned to d.
+            //c)	A value -1 will be assigned to d.
+            //d)	The code reports an error.
+            //e)	The code snippet will work correctly if ! is replaced by Not
+
+            #endregion
+
+            #region Q9: 9-	Which of the following is the correct output for the C# code given below?
+
+            //Console.WriteLine(13 / 2 + " " + 13 % 2); // 6  1
+
+            //a)	6.5 1
+            //b)	6.5 0
+            //c)	6 0
+            //d)	6 1
+            //e)	6.5 6.5
+
+            #endregion
+
+            #region Q10: What will be the output of the C# code given below?
+
+            //int num = 1, z = 5;
+
+            //if (!(num <= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z);
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+
+            #endregion
+
+            #endregion
+        }
+    }
+}
